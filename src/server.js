e252d395b11315b02d424d290ba109e18c8c3564
Vladimir Lebiga <lebiga@Mac.home>
2026-03-21 09:49:33 +0100

    updated backend 3

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 
 app.use(cors({
-  origin: ['http://127.0.0.1:5501', 'http://localhost:5501'],  
+  origin: ['http://127.0.0.1:5501', 'http://localhost:5501', 'https://vladimirlebiga.github.io/MedicalCards-2.0/'],  
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
 }));
 
